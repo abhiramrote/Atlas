de47f9b01b32d64871dcf8cb2afe3d5a420a17c5
Abhiram <107067728+abhiramrote@users.noreply.github.com>
2026-09-17 05:22:01 +0000

    M0: Initialize Atlas API and health endpoint

---
+++ apps/api/src/main/java/com/abhiram/atlas/AtlasApiApplication.java
@@ -0,0 +1,13 @@
+package com.abhiram.atlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasApiApplication.class, args);
+	}
+
+}
